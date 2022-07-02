c2f6cf606bfef778989a21c1dbcfaa1bd86891d2
Xavier <business.xsantos@gmail.com>
2022-07-02 03:42:04 -0700

    Grid layout

--- pages/404.js
+++ pages/404.js
@@ -3,32 +3,32 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 
 const NotFound = () => {
-  const router = useRouter();
-  const [seconds, setSeconds] = useState(5);
+    const router = useRouter();
+    const [seconds, setSeconds] = useState(5);
 
-  useEffect(() => {
-    if (seconds !== 0) {
-      setTimeout(() => {
-        setSeconds(seconds - 1);
-      }, 1000);
-    } else {
-      router.push("/");
-    }
-  });
+    useEffect(() => {
+        if (seconds !== 0) {
+            setTimeout(() => {
+                setSeconds(seconds - 1);
+            }, 1000);
+        } else {
+            router.push("/");
+        }
+    });
 
-  return (
-    <div className="not-found">
-      <h1>Ooops...</h1>
-      <h2>That page cannot be found.</h2>
-      <p>
-        Going back to the{" "}
-        <Link href="/">
-          <a>homepage</a>
-        </Link>
-        {" "}in{" "}{seconds}...
-      </p>
-    </div>
-  );
+    return (
+        <div className="not-found">
+            <h1>Ooops...</h1>
+            <h2>That page cannot be found.</h2>
+            <p>
+                Going back to the{" "}
+                <Link href="/">
+                    <a>homepage</a>
+                </Link>{" "}
+                in {seconds}...
+            </p>
+        </div>
+    );
 };
 
 export default NotFound;
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,50 @@
-import styles from '../styles/Home.module.css'
-import Link from 'next/link'
-import { Button } from '@nextui-org/react';
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
+import { Button, Container, Grid, Image } from "@nextui-org/react";
+import { Text, Spacer } from "@nextui-org/react";
 
 export default function Home() {
-  return (
-    <div>
-      <h1 className={styles.title}>Welcome to the Violet Verse</h1>
-      <p className={styles.text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-      <Link href="/about"><Button>Read More</Button></Link>
-      <Link href="/"><Button>Our Community</Button></Link>
-    </div>
-  )
+    return (
+        <div>
+            <Grid.Container gap={2} justify="center">
+                <Grid direction="column" xs={4}>
+                    <Text
+                        h1
+                        size={40}
+                        css={{
+                            textGradient:
+                                "45deg, $yellow600 -20%, $red600 100%",
+                        }}
+                        weight="bold"
+                    >
+                        Welcome to the Violet Verse
+                    </Text>
+                    <p className={styles.text}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqua.
+                    </p>
+                    <Container gap={0} css={{ d: "flex", flexWrap: "nowrap" }}>
+                        <Link href="/about">
+                            <Button>Read More</Button>
+                        </Link>
+                        <Spacer x={1} />
+                        <Link href="/">
+                            <Button color="secondary">Our Community</Button>
+                        </Link>
+                    </Container>
+                </Grid>
+                <Grid xs={8}>
+                    <Image
+                        showSkeleton
+                        width={763}
+                        height={338}
+                        maxDelay={10000}
+                        src="/Squared.png"
+                        alt="Default Image"
+                    />
+                </Grid>
+            </Grid.Container>
+        </div>
+    );
 }
