3c999e78ccfff2771995e0e6cdc3a881abd2a3ca
Sina <38741540+wxradar12@users.noreply.github.com>
2022-07-06 20:14:27 -0700

    Switched to MUI

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,21 @@
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
-import { Button, Container, Grid, Image, Spacer, Text, Row } from "@nextui-org/react";
+import { Grid } from "@mui/material";
+import { Button, Container, Image, Spacer, Text } from "@nextui-org/react";
+import { MultiSelectUnstyled } from "@mui/base";
 
 export default function Home() {
     return (
         <div>
-            <Grid.Container gap={2} justify="center">
-                <Grid direction="column" justify="center" xs={3}>
-
+            {/* First row of the homepage*/}
+            <Grid container direction="row" justify="center">
+                <Grid item justify="center" xs={3}>
+                    {/* Here is the color for the welcome part*/}
                     <Text
-                        h1
-                        size={40}
-                        css={{
-                            textGradient:
-                                "45deg, $yellow600 -20%, $red600 100%",
-                        }}
+                        h1k
+                        font-family="Work Sans"
+                        size={43}
+                        color="#293854"
                         weight="bold"
                     >
                         Welcome to the Violet Verse
@@ -25,7 +26,7 @@ export default function Home() {
                         magna aliqua.
                     </p>
 
-                    <Row wrap="wrap">
+                    <Grid wrap="wrap">
                         <Link href="/about">
                             <Button>Read More</Button>
                         </Link>
@@ -33,32 +34,32 @@ export default function Home() {
                         <Link href="/">
                             <Button color="secondary">Our Community</Button>
                         </Link>
-                        {/* Here is where the next section starts */}
-                        <Row>
-                        <Grid container spacing={4}></Grid>
-                        <Grid justify="space-between" xs={6}> </Grid>
-                        </Row>
-                        <Text
-                        h1k
-                        font-family= "Work Sans"
-                        size={43}
-                        color= "#f293854"
-                        weight="bold"
-                    >
-                        Curated Content Marketplace
-                    </Text>
-                    </Row>
+                    </Grid>
+                    <Grid item xs={9}>
+                        <Image
+                            width={763}
+                            height={338}
+                            src="/Squared.png"
+                            alt="Default Image"
+                        />
+                    </Grid>
+                    {/* Here is where the next section starts */}
+
+                    <Grid container spacing={4}></Grid>
+                    <Grid justify="space-between" xs={6}>
+                        {" "}
+                    </Grid>
                 </Grid>
-                <Grid xs={9}>
-                    <Image
-                        width={763
-						}
-                        height={338}
-                        src="/Squared.png"
-                        alt="Default Image"
-                    />
-                </Grid>
-            </Grid.Container>
+                <Text
+                    h1k
+                    font-family="Work Sans"
+                    size={43}
+                    color="#f293854"
+                    weight="bold"
+                >
+                    Curated Content Marketplace
+                </Text>
+            </Grid>
         </div>
     );
 }
