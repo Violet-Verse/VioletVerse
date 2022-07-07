6f059e6fcaf248ebd2bf558994dbb7ec52f45e25
Xavier <business.xsantos@gmail.com>
2022-07-06 21:05:23 -0700

    Grid direction

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,15 @@
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
 import Image from "next/image";
-import { Grid, Button } from "@mui/material";
+import { Grid, Button, ButtonGroup } from "@mui/material";
 import { Text } from "@nextui-org/react";
 
 export default function Home() {
     return (
         <div>
             {/* First Section */}
-            <Grid container direction="row" spacing={6} justifyContent="center">
-                <Grid item xs={3}>
+            <Grid container direction="row" spacing={6} justifyContent="space-around" alignItems="center">
+                <Grid item xs={6}>
                     <Text
                         h1k
                         font-family="Work Sans"
@@ -22,7 +22,12 @@ export default function Home() {
                     <p className={styles.text}>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                         sed do eiusmod tempor incididunt ut labore et dolore
-                        magna aliqua.
+                        magna aliqua. Strong, sweet, cup americano spoon blue
+                        mountain black robusta breve. Skinny variety to go white
+                        rich, redeye crema breve whipped. Strong, sweet, cup
+                        americano spoon blue mountain black robusta breve.
+                        Skinny variety to go white rich, redeye crema breve
+                        whipped.
                     </p>
                     <Grid container direction="row" spacing={2}>
                         <Grid item>
@@ -50,16 +55,33 @@ export default function Home() {
                 </Grid>
             </Grid>
             {/* Second Section */}
-            <Grid container>
-                <Text
-                    h1k
-                    font-family="Work Sans"
-                    size={43}
-                    color="#f293854"
-                    weight="bold"
-                >
-                    Curated Content Marketplace
-                </Text>
+            <Grid container justifyContent="space-between" alignItems="center" marginTop="50px">
+                <Grid item>
+                    <Text
+                        h1k
+                        font-family="Work Sans"
+                        size={43}
+                        color="#f293854"
+                        weight="bold"
+                    >
+                        Curated Content Marketplace
+                    </Text>
+                </Grid>
+                <Grid item xs={4}>
+                    <ButtonGroup
+                        variant="outlined"
+                        aria-label="outlined primary button group"
+                        size="medium"
+                        fullWidth="true"
+                    >
+                        <Button>Tech</Button>
+                        <Button>Lifestyle</Button>
+                        <Button>Education</Button>
+                    </ButtonGroup>
+                </Grid>
+                <Grid item>
+                    <Button>See All</Button>
+                </Grid>
             </Grid>
         </div>
     );
