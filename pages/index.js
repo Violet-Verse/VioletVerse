9e9505ce6be9b6f800596d5f149afb4f4c4dd928
Xavier <business.xsantos@gmail.com>
2022-07-08 16:28:23 -0700

    Homepage articles

--- pages/index.js
+++ pages/index.js
@@ -8,15 +8,15 @@ export default function Home() {
     return (
         <div>
             {/* First Section */}
-            <Grid container direction="row" spacing={6} justifyContent="space-around" alignItems="center">
+            <Grid
+                container
+                direction="row"
+                spacing={6}
+                justifyContent="space-around"
+                alignItems="center"
+            >
                 <Grid item xs={6}>
-                    <Text
-                        h1k
-                        font-family="Work Sans"
-                        size={43}
-                        color="#293854"
-                        weight="bold"
-                    >
+                    <Text h1 size={43} color="#f293854" weight="bold">
                         Welcome to the Violet Verse
                     </Text>
                     <p className={styles.text}>
@@ -31,19 +31,18 @@ export default function Home() {
                     </p>
                     <Grid container direction="row" spacing={2}>
                         <Grid item>
-                            <Link href="/about">
+                            <Link href="/">
                                 <Button variant="contained">Read More</Button>
                             </Link>
                         </Grid>
                         <Grid item>
-                            <Link href="/">
+                            <Link href="/about">
                                 <Button variant="outlined" color="secondary">
                                     Our Community
                                 </Button>
                             </Link>
                         </Grid>
                     </Grid>
-                    <Grid container spacing={4}></Grid>
                 </Grid>
                 <Grid item>
                     <Image
@@ -55,19 +54,18 @@ export default function Home() {
                 </Grid>
             </Grid>
             {/* Second Section */}
-            <Grid container justifyContent="space-between" alignItems="center" marginTop="50px">
-                <Grid item>
-                    <Text
-                        h1k
-                        font-family="Work Sans"
-                        size={43}
-                        color="#f293854"
-                        weight="bold"
-                    >
+            <Grid
+                container
+                justifyContent="space-between"
+                alignItems="center"
+                marginTop="100px"
+            >
+                <Grid item md={12} lg={6}>
+                    <Text h1 size={43} color="#f293854" weight="bold">
                         Curated Content Marketplace
                     </Text>
                 </Grid>
-                <Grid item xs={4}>
+                <Grid item md={6} lg={4}>
                     <ButtonGroup
                         variant="outlined"
                         aria-label="outlined primary button group"
@@ -80,7 +78,78 @@ export default function Home() {
                     </ButtonGroup>
                 </Grid>
                 <Grid item>
-                    <Button>See All</Button>
+                    <Button>[+] See All</Button>
+                </Grid>
+            </Grid>
+            <Grid container spacing={2} align="left" marginTop="25px">
+                <Grid item xs={12} sm={6} md={4}>
+                    <Image
+                        src="/Rectangle.png"
+                        alt="Placeholder Image"
+                        width={370}
+                        height={158}
+                    />
+                    <Text h3 color="#f293854" weight="bold">
+                        Article Title
+                    </Text>
+                    <Text color="#4D20A3" weight="semibold">
+                        Content Creator | Tech
+                    </Text>
+                    <Text
+                        color="#A4B0C0"
+                        weight="medium"
+                        css={{ maxWidth: "370px" }}
+                    >
+                        Strong, sweet, cup americano spoon blue mountain black
+                        robusta breve. Skinny variety to go white rich, redeye
+                        crema breve whipped.
+                    </Text>
+                </Grid>
+                <Grid item xs={12} sm={6} md={4}>
+                    <Image
+                        src="/Rectangle.png"
+                        alt="Placeholder Image"
+                        width={370}
+                        height={158}
+                    />
+                    <Text h3 color="#f293854" weight="bold">
+                        Article Title
+                    </Text>
+                    <Text color="#4D20A3" weight="semibold">
+                        Content Creator | Tech
+                    </Text>
+                    <Text
+                        color="#A4B0C0"
+                        weight="medium"
+                        css={{ maxWidth: "370px" }}
+                    >
+                        Strong, sweet, cup americano spoon blue mountain black
+                        robusta breve. Skinny variety to go white rich, redeye
+                        crema breve whipped.
+                    </Text>
+                </Grid>
+                <Grid item xs={12} sm={6} md={4}>
+                    <Image
+                        src="/Rectangle.png"
+                        alt="Placeholder Image"
+                        width={370}
+                        height={158}
+                    />
+                    <Text h3 color="#f293854" weight="bold">
+                        Article Title
+                    </Text>
+                    <Text color="#4D20A3" weight="semibold">
+                        Content Creator | Tech
+                    </Text>
+                    <Text
+                        color="#A4B0C0"
+                        weight="medium"
+                        css={{ maxWidth: "370px" }}
+                    >
+                        Strong, sweet, cup americano spoon blue mountain black
+                        robusta breve. Skinny variety to go white rich, redeye
+                        crema breve whipped.
+                    </Text>
                 </Grid>
             </Grid>
         </div>
