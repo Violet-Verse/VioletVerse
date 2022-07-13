b2da7eed0cb807cdbe4e33504e3a6fa2ab90a7fb
Xavier <business.xsantos@gmail.com>
2022-07-12 19:04:02 -0700

    Pages for each team member

---
+++ components/UserData.js
@@ -0,0 +1,34 @@
+export const members = [
+    {
+        name: "Mel",
+        id: 1,
+        title: "CEO",
+        facebook: "BMW",
+        twitter: "onDropParty",
+        linkedIn: "melissa-a-henderson-",
+    },
+    {
+        name: "Dani",
+        id: 2,
+        title: "COO",
+        facebook: "porsche",
+        twitter: "xavitime",
+        linkedIn: "melissa-a-henderson-",
+    },
+    {
+        name: "Ryan",
+        id: 3,
+        title: "CTO",
+        facebook: "Crunchyroll",
+        twitter: "ryanjsteffens",
+        linkedIn: "melissa-a-henderson-",
+    },
+    {
+        name: "Sina",
+        id: 4,
+        title: "CFO",
+        facebook: "appletv",
+        twitter: "TheVioletVerse",
+        linkedIn: "melissa-a-henderson-",
+    },
+];
