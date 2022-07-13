8aa8d6d9a95c63ce9474fd612e23decb9dfed40e
Sina <38741540+wxradar12@users.noreply.github.com>
2022-07-12 18:49:41 -0700

    added resources

---
+++ pages/resources.js
@@ -0,0 +1,24 @@
+import styles from "../styles/Home.module.css";
+import Image from "next/image";
+import { Grid, Button, ButtonGroup } from "@mui/material";
+import { Text } from "@nextui-org/react";
+
+const Resources = () => {
+
+
+
+    return (   
+        <div>
+
+
+
+
+
+
+
+        </div> 
+    );
+
+};
+
+export default Resources;
