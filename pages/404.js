0664bcd1e3e8d93c3c1646b3c5ff3ed63ff61133
Xavier <business.xsantos@gmail.com>
2022-07-19 04:56:26 -0700

    Updated 404 styling

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import Image from "next/image";
+import { Box } from "@mui/material";
 
 const NotFound = () => {
     const router = useRouter();
@@ -17,9 +19,23 @@ const NotFound = () => {
     });
 
     return (
-        <div className="not-found">
+        <Box
+            className="not-found"
+            sx={{
+                padding: { xs: "0px 5%", lg: "0px 20%", xl: "0px 20%" },
+            }}
+        >
             <h1>Ooops...</h1>
             <h2>That page cannot be found.</h2>
+            <Box sx={{ margin: "25px 0px" }}>
+                <Image
+                    width={1920}
+                    height={1080}
+                    src="/Photography_Banner.png"
+                    alt="Default Image"
+                    // layout="responsive"
+                />
+            </Box>
             <p>
                 Going back to the{" "}
                 <Link href="/">
@@ -27,7 +43,7 @@ const NotFound = () => {
                 </Link>{" "}
                 in {seconds}...
             </p>
-        </div>
+        </Box>
     );
 };
 
