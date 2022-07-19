3759a9c41202e368fda3d311dc784b85ea962f79
Xavier <business.xsantos@gmail.com>
2022-07-19 02:16:12 -0700

    testing header

--- components/Layout.js
+++ components/Layout.js
@@ -8,7 +8,7 @@ const Layout = ({ children }) => {
     return (
         <div>
             <Head>
-                <title>{siteTitle}</title>
+                <title>{siteTitle}: Testing12312312</title>
                 <link rel="icon" href="/favicon.ico" />
                 <meta
                     name="description"
@@ -16,9 +16,7 @@ const Layout = ({ children }) => {
                 />
                 <meta
                     property="og:image"
-                    content={`https://og-image.vercel.app/${encodeURI(
-                        siteTitle
-                    )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+                    content="https://i.imgur.com/HZmoUvQ.png"
                 />
                 <meta name="og:title" content={siteTitle} />
                 <meta name="twitter:card" content="summary_large_image" />
