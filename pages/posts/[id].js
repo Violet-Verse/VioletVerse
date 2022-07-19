3fd0bba97b7eb7401f0be95a9b472258503c4921
Xavier <business.xsantos@gmail.com>
2022-07-19 02:37:54 -0700

    testing headers

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -32,8 +32,19 @@ const Article = ({ posts }) => {
         <div>
             <Head>
                 <title>{posts.title + `| by Violet Verse`}</title>
-                <meta name="title" content={posts.title + `| by Violet Verse`} />
+                <meta
+                    name="title"
+                    content={posts.title + `| by Violet Verse`}
+                />
+                <meta
+                    name="og:title"
+                    content={posts.title + `| by Violet Verse`}
+                />
                 <meta name="description" content={posts.body} />
+                <meta
+                    property="og:image"
+                    content={`https://placehold.jp/40/693E9A/ffffff/1920x1080.png?text=Violet%20Verse%0ABy%20User${posts.userId}&css=%7B%22border-radius%22%3A%2215px%22%2C%22background%22%3A%22%20-webkit-gradient(linear%2C%20left%20top%2C%20left%20bottom%2C%20from(%23693E9A)%2C%20to(%23F985B4))%22%7D`}
+                />
             </Head>
             <h1>{posts.title}</h1>
             <p>{posts.body}</p>
