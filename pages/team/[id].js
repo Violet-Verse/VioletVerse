41d72cd9624ef17a158245a8a68bbe1bff11731e
Xavier <business.xsantos@gmail.com>
2022-07-19 22:09:40 -0700

    Head tags

--- pages/team/[id].js
+++ pages/team/[id].js
@@ -1,5 +1,6 @@
-import { Box, Grid } from "@mui/material";
 import Image from "next/image";
+import Head from "next/head";
+import { Box, Grid } from "@mui/material";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
@@ -23,81 +24,114 @@ export const getStaticPaths = async () => {
 };
 
 const Team = ({ member }) => {
+    const siteTitle = `${member.name} + | Violet Verse Team`;
+    const siteDescription = `${member.bio}`;
     return (
-        <Grid container direction="column" align="center" alignContent="center">
-            <h1>{member.name}</h1>
-            <h4>{member.title}</h4>
+        <>
+            <Head>
+                <meta name="og:title" content={siteTitle} />
+                <meta name="og:description" content={siteDescription} />
+                <meta
+                    property="og:image"
+                    content="https://i.imgur.com/HOcgWqo.png"
+                />
+                <meta property="og:image:type" content="image/png" />
+                <meta property="og:type" content="website" />
+                <meta name="twitter:site" content="@TheVioletVerse" />
+                <meta name="twitter:title" content={siteTitle} />
+                <meta name="twitter:description" content={siteDescription} />
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta
+                    name="twitter:image:src"
+                    content="https://i.imgur.com/HOcgWqo.png"
+                />
+            </Head>
             <Grid
                 container
-                direction="row"
+                direction="column"
                 align="center"
-                justifyContent="center"
+                alignContent="center"
             >
-                {member?.website && (
-                    <a href={member.website} target="_blank" rel="noreferrer">
-                        <LanguageIcon
-                            sx={{
-                                margin: "0px 3px",
-                                color: "#73839C",
-                            }}
-                        />
-                    </a>
-                )}
-                {member?.twitter && (
-                    <a
-                        href={`https://www.twitter.com/` + member.twitter}
-                        target="_blank"
-                        rel="noreferrer"
-                    >
-                        <TwitterIcon
-                            sx={{
-                                margin: "0px 3px",
-                                color: "#73839C",
-                            }}
-                        />
-                    </a>
-                )}
-                {member?.linkedIn && (
-                    <a
-                        href={`https://www.linkedin.com/in/` + member.linkedIn}
-                        target="_blank"
-                        rel="noreferrer"
-                    >
-                        <LinkedInIcon
-                            sx={{
-                                margin: "0px 3px",
-                                color: "#73839C",
-                            }}
-                        />
-                    </a>
-                )}
-                {member?.facebook && (
-                    <a
-                        href={`https://www.facebook.com/` + member.facebook}
-                        target="_blank"
-                        rel="noreferrer"
-                    >
-                        <FacebookIcon
-                            sx={{
-                                margin: "0px 3px",
-                                color: "#73839C",
-                            }}
-                        />
-                    </a>
-                )}
+                <h1>{member.name}</h1>
+                <h4>{member.title}</h4>
+                <Grid
+                    container
+                    direction="row"
+                    align="center"
+                    justifyContent="center"
+                >
+                    {member?.website && (
+                        <a
+                            href={member.website}
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            <LanguageIcon
+                                sx={{
+                                    margin: "0px 3px",
+                                    color: "#73839C",
+                                }}
+                            />
+                        </a>
+                    )}
+                    {member?.twitter && (
+                        <a
+                            href={`https://www.twitter.com/` + member.twitter}
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            <TwitterIcon
+                                sx={{
+                                    margin: "0px 3px",
+                                    color: "#73839C",
+                                }}
+                            />
+                        </a>
+                    )}
+                    {member?.linkedIn && (
+                        <a
+                            href={
+                                `https://www.linkedin.com/in/` + member.linkedIn
+                            }
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            <LinkedInIcon
+                                sx={{
+                                    margin: "0px 3px",
+                                    color: "#73839C",
+                                }}
+                            />
+                        </a>
+                    )}
+                    {member?.facebook && (
+                        <a
+                            href={`https://www.facebook.com/` + member.facebook}
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            <FacebookIcon
+                                sx={{
+                                    margin: "0px 3px",
+                                    color: "#73839C",
+                                }}
+                            />
+                        </a>
+                    )}
+                </Grid>
+                <Image
+                    width={345.13}
+                    height={400}
+                    src={member.photo}
+                    alt="Default Image"
+                    className="imageSm"
+                    objectFit={"contain"}
+                />
+                <Box sx={{ padding: "0px 15%" }}>
+                    <p>{member.bio}</p>
+                </Box>
             </Grid>
-            <Image
-                width={345.13}
-                height={400}
-                src={member.photo}
-                alt="Default Image"
-                className="imageSm"
-                objectFit={"contain"}
-            />
-            <Box sx={{ padding: "0px 15%" }}>
-                <p>{member.bio}</p>
-            </Box>
-        </Grid>
+        </>
     );
 };
 
