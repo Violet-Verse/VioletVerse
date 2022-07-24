2c78fdc57f1cd518166edb4744ebe2fc66dee8ed
Xavier <business.xsantos@gmail.com>
2022-07-23 18:48:57 -0700

    Post title

--- pages/editor.js
+++ pages/editor.js
@@ -1,3 +1,4 @@
+import { Button, Grid, TextField } from "@mui/material";
 import React from "react";
 import Editor from "../components/Editor";
 
@@ -6,7 +7,33 @@ const EditorPage = () => {
         "<h1>Into the Violet Verse</h1><p>Start writing your post here.</p>";
     const [value, setValue] = React.useState(initialValue);
 
-    return <Editor value={value} onChange={setValue} />;
+    return (
+        <>
+            <Grid container direction="row" sx={{ mb: 4 }}>
+                <Grid item sx={{ display: "flex", flexGrow: 1 }}>
+                    <TextField
+                        variant="outlined"
+                        label="Post Title"
+                        fullWidth
+                        autoFocus
+                    />
+                </Grid>
+                <Grid item sx={{ display: "flex", flexGrow: 0, ml: 4 }}>
+                    <Button variant="contained">Submit</Button>
+                </Grid>
+            </Grid>
+            <Editor
+                value={value}
+                onChange={setValue}
+                controls={[
+                    ["bold", "italic", "underline", "link", "image"],
+                    ["unorderedList", "h1", "h2", "h3"],
+                    ["sup", "sub"],
+                ]}
+            />
+            {value}
+        </>
+    );
 };
 
 export default EditorPage;
