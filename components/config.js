63c5bec67f5a375ac5d0556bde1ae137053b050f
Xavier <business.xsantos@gmail.com>
2022-07-25 16:56:15 -0700

    updated config for api

--- components/config.js
+++ components/config.js
@@ -1,5 +1,1 @@
-const dev = process.env.NODE_ENV !== "production" || "preview";
-
-export const server = dev
-    ? "http://localhost:3000/"
-    : "https://violet-verse-chi.vercel.app/";
+export const server = process.env.NEXT_PUBLIC_API;
