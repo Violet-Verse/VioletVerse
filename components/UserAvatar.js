14a3e869c20b84d2e09576b6d00be914201393ee
Xavier <business.xsantos@gmail.com>
2022-08-08 11:48:00 -0700

    Fixed avatar load glitch

--- components/UserAvatar.js
+++ components/UserAvatar.js
@@ -5,13 +5,13 @@ const UserAvatar = (props) => {
     const user = props.user;
     return (
         <>
-            {user?.picture && (
+            {user?.picture && user && (
                 <Avatar
                     alt={user?.name || "user picture"}
                     src={user?.picture}
                 />
             )}
-            {!user?.picture && (
+            {!user?.picture && user && (
                 <Box sx={{ pt: 0.5 }}>
                     <Jazzicon diameter={40} seed={user?.uniqueId} />
                 </Box>
