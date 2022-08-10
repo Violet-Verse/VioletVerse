e51688520c1b74711bb6e88da9651e140861daea
Xavier <business.xsantos@gmail.com>
2022-08-10 14:55:28 -0700

    Progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,21 @@ import { useUser } from "../hooks/useAuth";
 import "../styles/fonts.css";
 import "../styles/globals.css";
 
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+import Router from "next/router";
+
+NProgress.configure({
+    minimum: 0.3,
+    easing: "ease",
+    speed: 800,
+    showSpinner: false,
+});
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
+
 function MyApp({ Component, pageProps }) {
     const { user } = useUser();
 
