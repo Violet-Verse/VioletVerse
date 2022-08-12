58eaaf758fe11a1cf68c6fc2881cbb9ced59f604
Xavier <business.xsantos@gmail.com>
2022-08-12 03:10:13 -0700

    SEO tags

--- pages/user/[username].js
+++ pages/user/[username].js
@@ -1,5 +1,6 @@
 import ProfileLayout from "../../components/Profile/ProfileLayout";
 import { getUserByUsername } from "../api/database/getUser";
+import Head from "next/head";
 
 export async function getServerSideProps(context) {
     const username = context.params.username;
@@ -17,7 +18,30 @@ export async function getServerSideProps(context) {
 }
 
 const UserProfile = ({ user }) => {
-    return <ProfileLayout user={user} />;
+    const siteTitle = `${user?.name} - Profile | Violet Verse`;
+    const metaTitle = `${user?.name} - Profile | Violet Verse`;
+    const siteImage = user?.picture;
+    const siteDescription = user?.bio;
+    return (
+        <>
+            <Head>
+                <title>{siteTitle}</title>
+                <meta name="og:title" content={metaTitle} />
+                <meta name="og:description" content={siteDescription} />
+                <meta property="og:image" content={siteImage} />
+                <meta property="og:image:type" content="image/png" />
+                <meta property="og:image:width" content="800" />
+                <meta property="og:image:height" content="420" />
+                <meta property="og:type" content="website" />
+                <meta name="twitter:site" content="@TheVioletVerse" />
+                <meta name="twitter:title" content={metaTitle} />
+                <meta name="twitter:description" content={siteDescription} />
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:image:src" content={siteImage} />
+            </Head>
+            <ProfileLayout user={user} />;
+        </>
+    );
 };
 
 export default UserProfile;
