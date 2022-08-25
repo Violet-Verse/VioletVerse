362cce404b07fd997aad3373169a330552ea6acd
Xavier <business.xsantos@gmail.com>
2022-08-25 02:59:10 -0700

    debug

--- pages/api/utils/awsConfig.js
+++ pages/api/utils/awsConfig.js
@@ -1,7 +1,7 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 
 export const ddbClient = new DynamoDBClient({
-    region: process.env.VV_AWS_REGION,
-    accessKeyId: process.env.VV_AWS_ACCESS_KEY,
-    secretAccessKey: process.env.VV_AWS_SECRET_ACCESS_KEY,
+    region: process.env.REGION,
+    accessKeyId: process.env.ACCESS_KEY,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
 });
