eddb2228d609efa6d9530d2cdfa654523712a414
Xavier <business.xsantos@gmail.com>
2022-09-11 20:07:50 -0700

    changed vv funding wallet

--- cadence/scripts/transactions/purchaseContent.js
+++ cadence/scripts/transactions/purchaseContent.js
@@ -8,7 +8,7 @@ transaction(contributorAddress: Address, amountVV: UFix64, amountContributor: UF
     let signerVault = acct.borrow<&VioletVerse.Vault>(from: /storage/violetVerseVault)
                         ?? panic("Couldn't get the signer's Vault")
 
-    let vvVault = getAccount(0x2793bbf3c8ffa284).getCapability(/public/violetVerseBalance)
+    let vvVault = getAccount(0xca37d2f49cae6068).getCapability(/public/violetVerseBalance)
                           .borrow<&VioletVerse.Vault{FungibleToken.Receiver}>()
                           ?? panic("Couldn't get the public Vault of vv.")
 
