4c6445c1d9cd1bfc2e61c48f80f6a25dea4730e6
Xavier <business.xsantos@gmail.com>
2022-12-03 03:37:14 -0800

    meta tag update

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,12 @@
 import { Box, Grid } from "@mui/material";
+import Head from "next/head";
 import Image from "next/image";
 
 const Contact = () => {
+    const siteTitle = `Contact Us | Violet Verse`;
+    const metaTitle = `Contact Us`;
+    const siteDescription = `Reach the Violet Verse team at gm@violetverse.io`;
+    const siteImage = "https://i.imgur.com/LFpaItV.png";
     return (
         <Box
             sx={{
@@ -14,6 +19,21 @@ const Contact = () => {
                 },
             }}
         >
+            <Head>
+                <title>{siteTitle}</title>
+                <meta name="og:title" content={metaTitle} />
+                <meta name="og:description" content={siteDescription} />
+                <meta property="og:image" content={siteImage} />
+                <meta property="og:image:type" content="image/png" />
+                <meta property="og:image:width" content="80" />
+                <meta property="og:image:height" content="80" />
+                <meta property="og:type" content="website" />
+                <meta name="twitter:site" content="@TheVioletVerse" />
+                <meta name="twitter:title" content={metaTitle} />
+                <meta name="twitter:description" content={siteDescription} />
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:image:src" content={siteImage} />
+            </Head>
             <Grid
                 container
                 direction="column"
