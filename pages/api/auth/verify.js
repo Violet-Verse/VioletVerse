314e3ccc6f6ceb30dc7e6c28577b8eb371afe440
Xavier Santos <business.xsantos@gmail.com>
2023-06-08 03:21:16 -0700

    sd reg

--- pages/api/auth/verify.js
+++ pages/api/auth/verify.js
@@ -1,10 +1,3 @@
-import * as fcl from "@onflow/fcl";
-import Iron from "@hapi/iron";
-import CookieService from "../../../lib/cookie";
-import { table } from "../utils/userTable";
-import { deleteNonce, getNonce } from "../aws/nonceControl";
-import fetch from "node-fetch";
-
 export default async function handler(req, res) {
     const data = req.body;
     const nonce = data.nonce;
@@ -50,72 +43,6 @@ export default async function handler(req, res) {
                             Iron.defaults
                         );
                         CookieService(res, token);
-
-                        // Check if user exists on Stardust.gg
-                        const stardustUrl = `https://core-api.stardust.gg/v1/player/get?playerId=${user.address}`;
-                        const stardustOptions = {
-                            method: "GET",
-                            headers: {
-                                accept: "application/json",
-                                "x-api-key":
-                                    "n8ZJVlZfAL1it1SkcNB538CFxRDIDL1aaJ9lE7Lt",
-                            },
-                        };
-
-                        fetch(stardustUrl, stardustOptions)
-                            .then((stardustRes) => stardustRes.json())
-                            .then((stardustJson) => {
-                                // Check if user exists in Stardust.gg response
-                                if (stardustJson) {
-                                    // User exists in Stardust.gg
-                                    return res.status(200).json({
-                                        userData: users,
-                                        registration: false,
-                                        stardustData: stardustJson,
-                                    });
-                                } else {
-                                    // User does not exist in Stardust.gg -- ADD NEW USER
-                                    table.create(
-                                        [
-                                            {
-                                                fields: {
-                                                    userId: `${user.address}`,
-                                                    email: `${user.email}`,
-                                                    role: "user",
-                                                    flowAddress: `${req.body.address}`,
-                                                    username: `${req.body.address}`,
-                                                    name: `${req.body.address}`,
-                                                },
-                                            },
-                                        ],
-                                        async function (err, records) {
-                                            if (err) {
-                                                console.error(err);
-                                                return res.status(400).end();
-                                            }
-                                            const token = await Iron.seal(
-                                                user,
-                                                process.env.TOKEN_SECRET,
-                                                Iron.defaults
-                                            );
-                                            CookieService(res, token);
-                                            return res.status(200).json({
-                                                userData: records[0].fields,
-                                                registration: true,
-                                                stardustData: null,
-                                            });
-                                        }
-                                    );
-                                }
-                            })
-                            .catch((err) => {
-                                console.error("Stardust.gg error:", err);
-                                return res.status(200).json({
-                                    userData: users,
-                                    registration: false,
-                                    stardustData: null,
-                                });
-                            });
                     } else {
                         // User not found in database -- ADD NEW USER
                         try {
@@ -143,52 +70,86 @@ export default async function handler(req, res) {
                                         Iron.defaults
                                     );
                                     CookieService(res, token);
-
-                                    // Create new player in Stardust.gg
-                                    const stardustUrl =
-                                        "https://core-api.stardust.gg/v1/player/create";
-                                    const stardustOptions = {
-                                        method: "POST",
-                                        headers: {
-                                            accept: "application/json",
-                                            "content-type": "application/json",
-                                            "x-api-key":
-                                                "n8ZJVlZfAL1it1SkcNB538CFxRDIDL1aaJ9lE7Lt",
-                                        },
-                                        body: JSON.stringify({
-                                            userData: { email: user.email },
-                                            uniqueId: user.address,
-                                        }),
-                                    };
-
-                                    fetch(stardustUrl, stardustOptions)
-                                        .then((stardustRes) =>
-                                            stardustRes.json()
-                                        )
-                                        .then((stardustJson) => {
-                                            return res.status(200).json({
-                                                userData: records[0].fields,
-                                                registration: true,
-                                                stardustData: stardustJson,
-                                            });
-                                        })
-                                        .catch((err) => {
-                                            console.error(
-                                                "Stardust.gg error:",
-                                                err
-                                            );
-                                            return res.status(200).json({
-                                                userData: records[0].fields,
-                                                registration: true,
-                                                stardustData: null,
-                                            });
-                                        });
                                 }
                             );
                         } catch (err) {
                             console.log(err);
                         }
                     }
+
+                    // Check if user exists on Stardust.gg
+                    const stardustUrl = `https://core-api.stardust.gg/v1/player/get?playerId=${user.address}`;
+                    const stardustOptions = {
+                        method: "GET",
+                        headers: {
+                            accept: "application/json",
+                            "x-api-key":
+                                "n8ZJVlZfAL1it1SkcNB538CFxRDIDL1aaJ9lE7Lt",
+                        },
+                    };
+
+                    fetch(stardustUrl, stardustOptions)
+                        .then((stardustRes) => stardustRes.json())
+                        .then((stardustJson) => {
+                            // Check if user exists in Stardust.gg response
+                            if (stardustJson) {
+                                // User exists in Stardust.gg
+                                return res.status(200).json({
+                                    userData: users,
+                                    registration: false,
+                                    stardustData: stardustJson,
+                                });
+                            } else {
+                                // User does not exist in Stardust.gg, so create new player
+                                const stardustCreateUrl =
+                                    "https://core-api.stardust.gg/v1/player/create";
+                                const stardustCreateOptions = {
+                                    method: "POST",
+                                    headers: {
+                                        accept: "application/json",
+                                        "content-type": "application/json",
+                                        "x-api-key":
+                                            "n8ZJVlZfAL1it1SkcNB538CFxRDIDL1aaJ9lE7Lt",
+                                    },
+                                    body: JSON.stringify({
+                                        userData: { email: user.email },
+                                        uniqueId: user.address,
+                                    }),
+                                };
+
+                                fetch(stardustCreateUrl, stardustCreateOptions)
+                                    .then((stardustCreateRes) =>
+                                        stardustCreateRes.json()
+                                    )
+                                    .then((stardustCreateJson) => {
+                                        return res.status(200).json({
+                                            userData: users,
+                                            registration: true,
+                                            stardustData: stardustCreateJson,
+                                        });
+                                    })
+                                    .catch((err) => {
+                                        console.error(
+                                            "Stardust.gg error:",
+                                            err
+                                        );
+                                        return res.status(200).json({
+                                            userData: users,
+                                            registration: true,
+                                            stardustData: null,
+                                        });
+                                    });
+                            }
+                        })
+                        .catch((err) => {
+                            console.error("Stardust.gg error:", err);
+                            return res.status(200).json({
+                                userData: users,
+                                registration: false,
+                                stardustData: null,
+                            });
+                        });
+
                     if (err) {
                         console.error(err);
                         return;
