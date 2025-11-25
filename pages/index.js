db38e6a54279cad89ee52ddbc4c321fd9b2b7b18
zkxp-admin <info@zkxp.xyz>
2025-11-24 17:45:02 -0700

    Revert "Redesign navbar with floating header and mobile menu"
    This reverts commit 07cf5bf4b8db4a2bd7c63454ca6e6bbd0b27cf4a.

--- pages/index.js
+++ pages/index.js
@@ -36,7 +36,7 @@ const Home = ({ posts, authors, contributors }) => {
     };
 
     return (
-        <Box>
+        <Box sx={{ mt: -4 }}>
             <Head>
                 <meta
                     property="og:image"
