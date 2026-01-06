a09f1eca07b0f6753ce6d5599d5d75f397acabe1
Violet Summer <95707829+violetsummerzine@users.noreply.github.com>
2026-01-06 20:01:13 +0000

    fix FCL confif for server-side rendering

--- flow/config.js
+++ flow/config.js
@@ -1,20 +1,23 @@
 import * as fcl from "@onflow/fcl";
 
-const resolver = async () => {
-    const response = await fetch("/api/auth/generate");
-    const { nonce } = await response.json();
-    return {
-        appIdentifier: "VioletVerse",
-        nonce,
+// Only configure FCL on the client side (browser)
+if (typeof window !== 'undefined') {
+    const resolver = async () => {
+        const response = await fetch("/api/auth/generate");
+        const { nonce } = await response.json();
+        return {
+            appIdentifier: "VioletVerse",
+            nonce,
+        };
     };
-};
 
-fcl.config({
-    "app.detail.title": "Violet Verse",
-    "app.detail.icon": "https://i.imgur.com/jDJnCzx.png",
-    "accessNode.api": "https://rest-mainnet.onflow.org",
-    "0xVioletVerse": "0xf5f7db710acb59d3",
-    "discovery.wallet": "https://fcl-discovery.onflow.org/authn", // Updated for standard Flow wallets
-    "fcl.accountProof.resolver": resolver,
-    "discovery.wallet.method": "HTTP/POST"
-});
+    fcl.config({
+        "app.detail.title": "Violet Verse",
+        "app.detail.icon": "https://i.imgur.com/jDJnCzx.png",
+        "accessNode.api": "https://rest-mainnet.onflow.org",
+        "0xVioletVerse": "0xf5f7db710acb59d3",
+        "discovery.wallet": "https://fcl-discovery.onflow.org/authn",
+        "fcl.accountProof.resolver": resolver,
+        "discovery.wallet.method": "HTTP/POST"
+    });
+}
