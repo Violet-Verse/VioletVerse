138b73614b8a26e24aa269806babdbb4d1d94fda
Violet Summer <95707829+violetsummerzine@users.noreply.github.com>
2026-01-28 17:23:42 -0500

    Refactor next.config.js configuration structure

--- next.config.js
+++ next.config.js
@@ -1,39 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    images: {
-        domains: [
-            "i.imgur.com",
-            "i.ibb.co",
-            "img.youtube.com",
-            "cdn.discordapp.com",
-            "www.stardust.gg",
-            "pbs.twimg.com",
-        ],
-    },
-    
-    webpack: (config, { isServer }) => {
-        // Completely exclude Flow FCL from server bundle
-        if (isServer) {
-            config.externals = config.externals || [];
-            config.externals.push({
-                '@onflow/fcl': 'commonjs @onflow/fcl',
-                '@onflow/types': 'commonjs @onflow/types'
-            });
-        }
+      reactStrictMode: true,
+      swcMinify: true,
+      images: {
+              domains: [
+                        "i.imgur.com",
+                        "i.ibb.co",
+                        "img.youtube.com",
+                        "cdn.discordapp.com",
+                        "www.stardust.gg",
+                        "pbs.twimg.com",
+                      ],
+      },
 
-        // Handle newer JavaScript syntax for Privy dependencies
+      webpack: (config, { isServer }) => {
+              // Handle newer JavaScript syntax for Privy dependencies
         config.module.rules.push({
-            test: /\.m?js$/,
-            type: 'javascript/auto',
-            resolve: {
-                fullySpecified: false,
-            },
+                  test: /\.m?js$/,
+                  type: 'javascript/auto',
+                  resolve: {
+                              fullySpecified: false,
+                  },
         });
 
         return config;
-    },
+      },
 };
 
 module.exports = nextConfig;
